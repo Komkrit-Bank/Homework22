0354cd5ae768ee95bf32f0f47d3193c196e11ff2
Rokoshan <133026403+Komkrit-Bank@users.noreply.github.com>
2023-05-29 12:28:14 +0700

    Header Component

---
+++ myHomework/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Button from "@mui/material/Button/Button"
+import Stack  from "@mui/material/Stack"
+import { Box } from "@mui/material"
+
+const Header = () => {
+  return (
+    <>
+    <Box
+        display="flex"
+        flexDirection='column'
+        alignItems='center'
+        maxWidth='sm'>
+        <h1>Album Layout</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione magnam iste, necessitatibus, harum eligendi distinctio cum, quasi inventore eos maxime molestias. Quod corrupti harum cumque officia hic dolore ipsa exercitationem.</p>
+        <Stack spacing={2} direction="row">
+            <Button variant="contained">MAIN CALL TO ACTION</Button>
+            <Button variant="outlined">SECONDY ACTION</Button>
+        </Stack>
+    </Box>
+    </>
+  )
+}
+
+export default Header
